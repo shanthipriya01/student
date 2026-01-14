dc8b8fed6540c10cfdc33d82eb5a6fb6f11d51f6
N.shanthi priya <SHANTHIPRIYANANDAGIRI01@GMAIL.COM>
2026-01-14 17:44:44 +0530

    Implement student management functions in script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+class Student:
+    def _init_(self, id, name):
+        self.id = id
+        self.name = name
+
+students = []
+
+def add_student(id, name):
+    students.append(Student(id, name))
+    print("Student added")
+
+def show_students():
+    for s in students:
+        print(s.id, s.name)
+
+add_student(1, "Priya")
+add_student(2, "Ravi")
+show_students()
